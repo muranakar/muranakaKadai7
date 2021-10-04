d87e6e82c9d0e6765446a7d4be6ce97075ee8027
akio0911 <akio0911@gmail.com>
2021-10-05 08:19:26 +0900

    feedback

--- muranakaKadai7/Controller/AdditionViewController.swift
+++ muranakaKadai7/Controller/AdditionViewController.swift
@@ -31,17 +31,7 @@ class AdditionViewController: UIViewController, UITextFieldDelegate {
         return true
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    @IBAction func didTap(_ sender: Any) {
+        view.endEditing(true)
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
 }
